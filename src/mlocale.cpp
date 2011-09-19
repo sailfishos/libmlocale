b9aa63c4360811e84779d9473b585d217c4320d6
Mike FABIAN <mike.fabian@basyskom.de>
2011-09-19 17:10:13 +0200

    Fixes: NB#277314 - Regional format; Translation improvement for "stroke" in Hong Kong Chinese.
    RevBy: Holger Schröder
    MR: 2248
    
    Details:
    
    Use 劃 instead of 筆 as the label for the stroke count index
    labels always.
    
    Attention: when this is integrated, a change in libcontacts
    becomes necessary as libcontacts still expects 筆.

--- src/mlocale.cpp
+++ src/mlocale.cpp
@@ -3797,7 +3797,7 @@ QString MLocale::indexBucket(const QString &str, const QStringList &buckets, con
             }
             else {
                 if(buckets.first() == QString::fromUtf8("一")) // stroke count sorting
-                    return QString::number(i)+QString::fromUtf8("筆");
+                    return QString::number(i)+QString::fromUtf8("劃");
                 else
                     return buckets[i-1];
             }
